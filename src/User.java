cad301de1a2d589201972f814670ca938083defb
shoham sofer <92480018+sofer1445@users.noreply.github.com>
2021-12-26 10:40:35 +0200

    https://github.com/sofer1445/homeWork4/blob/master/src/RealEstate.java

--- src/User.java
+++ src/User.java
@@ -2,10 +2,10 @@ public class User {
 
     private String userName;
     private String password;
-    private int number;
+    private String number;
     private boolean typeUser;
 
-    public User (String username, String password,int number, boolean typeUser) {
+    public User (String username, String password,String number, boolean typeUser) {
         this.userName = username;
         this.password = password;
         this.number = number;
